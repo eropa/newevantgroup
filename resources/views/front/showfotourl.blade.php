4f5698f33629a5ef93992d295af01aa6e383b21f
Igor Lutsenko <eropamd@gmail.com>
2020-11-27 00:30:03 +0200

    fb public

--- resources/views/front/showfotourl.blade.php
+++ resources/views/front/showfotourl.blade.php
@@ -3,6 +3,7 @@
 @section('metateg')
     <meta property="og:url"         content="{{ route('page.news.showfotourl',['id'=>$fotoData->id]) }}">
     <meta property="og:title"       content="Мебель на заказ в г.Рыбница">
+    <meta property="fb:app_id"       content="148073311954254">
     <meta property="og:type"        content="article" />
     <meta property="og:description" content="Мебель на заказ в г.Рыбница">
     <meta property="og:image"       content="{{ asset('gallary/'.$fotoData->file_name)}}">
