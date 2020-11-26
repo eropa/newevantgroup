e1ee505264dd9e6f78284a28b6320b465e2258c5
Igor Lutsenko <eropamd@gmail.com>
2020-11-27 00:21:56 +0200

    public foto

--- resources/views/front/showfotourl.blade.php
+++ resources/views/front/showfotourl.blade.php
@@ -1,11 +1,12 @@
 @extends('front.app')
 
 @section('metateg')
-    <meta name="og:url" content="{{ route('page.news.showfotourl',['id'=>$fotoData->id]) }}">
-    <meta name="og:title" content="Мебель на заказ в г.Рыбница">
-    <meta name="og:description" content="Мебель на заказ в г.Рыбница">
-    <meta name="og:image" content="{{ asset('gallary/'.$fotoData->file_name)}}">
-    <meta name="og:image:alt" content="{{ $fotoData->name}}">
+    <meta property="og:url" content="{{ route('page.news.showfotourl',['id'=>$fotoData->id]) }}">
+    <meta property="og:title" content="Мебель на заказ в г.Рыбница">
+    <meta property="og:type"  content="article" />
+    <meta property="og:description" content="Мебель на заказ в г.Рыбница">
+    <meta property="og:image" content="{{ asset('gallary/'.$fotoData->file_name)}}">
+    <meta property="og:image:alt" content="{{ $fotoData->name}}">
 @endsection
 
 @section('content')
